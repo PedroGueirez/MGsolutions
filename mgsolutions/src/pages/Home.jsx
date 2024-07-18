8e56d9ff8f74f423763d99272a0b3a1d5f362902
pedro <pedro.gueirez@gmail.com>
2024-07-18 19:55:19 +0100

    init homePage

--- mgsolutions/src/pages/Home.jsx
+++ mgsolutions/src/pages/Home.jsx
@@ -1,14 +1,57 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaBoltLightning } from "react-icons/fa6";
+import { IoHomeSharp } from "react-icons/io5";
+import { MdPhonelink } from "react-icons/md";
+import img1 from '../assets/img3-telecomunicaçoes.jpg'
 
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        <div>
-           <p>oiiii</p>
-        </div>
-        
+            <div className="flex  h-full w-full mt-20  ">
+
+                <div className="w-2/5 bg-white shadow- rounded-lg p-6 mr-4 border-2 border-blue-500 pl-10">
+                    <h1 className="text-3xl  mb-3">Olá,</h1>
+
+                    <h2 className="text-4xl font-serif  mb-12">Em que podemos ajudar hoje?</h2>
+
+                    <ul className=" pl-1 space-y-10">
+                        <li className=" text-xl  flex items-center ">
+                            <MdPhonelink className="mr-5" />
+                            <Link to="/telecomunicacoes" className="text-black border-b border-gray-600">
+                                Telecomunicações
+                            </Link>
+                        </li>
+                        <li className="text-xl  flex items-center ">
+                            <FaBoltLightning className="mr-5" />
+                            <Link to="/eletricidade" className="text-black border-b border-gray-600">
+                                Eletricidade
+                            </Link>
+                        </li>
+                        <li className="text-xl flex items-center">
+                            <IoHomeSharp className="mr-5" />
+                            <Link to="/inteligentes" className="text-black border-b border-gray-600">
+                                Inteligentes
+                            </Link>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div className="w-3/5 bg-white shadow-md rounded-lg p-6 border-2 border-blue-500">
+                    <div className="flex justify-center">
+                        <img src={img1} alt="Imagem Telecomunicaçoes" className="w-full h-full"  />
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </>
-    )
+    );
 }
+
+
 export default Home
--- mgsolutions/tailwind.config.js
+++ mgsolutions/tailwind.config.js
@@ -11,6 +11,7 @@ export default {
       md:'768px',
       lg:'1024px',
     },
+  
     extend: {},
   },
   plugins: [],
