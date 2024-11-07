6b9e8244a411c49a334caff651a4473fa3d42bfd
MMgsolutionsG Solutions <mgsolutions@Air-de-MG.home>
2024-11-07 19:49:25 +0000

    footer

--- mgsolutions/src/components/Footer.jsx
+++ mgsolutions/src/components/Footer.jsx
@@ -1,75 +1,86 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { FaInstagram } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa6";
 import { FaTelegram } from "react-icons/fa";
 
+function Footer() {
+  return (
+    <div className=" bg-slate-950 p-8 grid md:grid-cols-3 gap-8 text-white p pb-8">
+      <div>
+        <h1 className=" w-full text-3xl font-mono ml-2">MGsolutions</h1>
+        <p className="py-4 ml-2">
+          Um sitio onde todas as necessidades podem ser satisfeitas
+        </p>
+        <div className=" ml-2 flex justify-between sm: w-[70%] my-4 ">
+          <FaFacebookSquare size={25} />
+          <FaInstagram size={25} />
+          <FaWhatsapp size={25} />
+          <FaTelegram size={25} />
+        </div>
+      </div>
 
-function Footer(){
+      <div className="md:col-span-2 flex justify-between">
+        <div>
+          <h6 className="py-1 font-mono text-xl text-gray-400 ">Serviços</h6>
+          <ul>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">
+              Telecomunicações
+            </li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">
+              Energia e Eletricidade
+            </li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">
+              Sistemas de Alarmes e Segurança
+            </li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">
+              Casas Inteligentes
+            </li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">
+              Planos e Pacotes
+            </li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">
+              Instalações e Manuntenções
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="py-1 font-mono text-xl text-gray-400">
+            Suporte ao Cliente
+          </h6>
+          <ul>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Central de Atendimento</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Perguntas Frequentes (FAQ)</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">Politicas de Privacidade</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-sky-700">Garantias e Reembolsos</li>
+          </ul>
+        </div>
 
-    return (
-        
-        <div className=" bg-slate-950 p-8 grid md:grid-cols-3 gap-8 text-white p pb-8">
-            <div>
-                <h1 className=" w-full text-3xl font-mono ml-2">MGsolutions</h1>
-                <p className="py-4 ml-2">Um sitio onde todas as necessidades podem ser satisfeitas</p>
-                <div className=" ml-2 flex justify-between sm: w-[70%] my-4 "> 
-                     <FaFacebookSquare size={25}/>
-                     <FaInstagram size={25}/>
-                     <FaWhatsapp size={25}/>
-                     <FaTelegram size={25}/>
-                </div>
-            </div>
-            
-            <div className="md:col-span-2 flex justify-between">
-                <div>
-                    <h6 className="py-1 font-mono text-xl text-gray-400 ">Serviços</h6>
-                    <ul>
-                        <li className="py-2 text-sm">Telecomunicações</li>
-                        <li className="py-2 text-sm">Energia e Eletricidade</li>
-                        <li className="py-2 text-sm">Sistemas de Alarmes e Segurança</li>
-                        <li className="py-2 text-sm">Casas Inteligentes</li>
-                        <li className="py-2 text-sm">Planos e Pacotes</li>
-                        <li className="py-2 text-sm">Instalações e Manuntenções</li>
-                    </ul>
-                </div>
-                <div>
-                    <h6 className="py-1 font-mono text-xl text-gray-400">Suporte ao Cliente</h6>
-                    <ul>
-                        <li className="py-2 text-sm">Central de Atendimento</li>
-                        <li className="py-2 text-sm">Perguntas Frequentes (FAQ)</li>
-                        <li className="py-2 text-sm">Politicas de Privacidade</li>
-                        <li className="py-2 text-sm">Garantias e Reembolsos</li>
-                    </ul>
-                </div>
+        <div>
+          <h6 className="py-1 font-mono text-xl text-gray-400">
+            Institucional
+          </h6>
+          <ul>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Sobre Nós</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Nossa Missão e Visão</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Equipa</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Parceiros</li>
+          </ul>
+        </div>
 
-                <div>
-                    <h6 className="py-1 font-mono text-xl text-gray-400">Institucional</h6>
-                    <ul>
-                        <li className="py-2 text-sm">Sobre Nós</li>
-                        <li className="py-2 text-sm">Nossa Missão e Visão</li>
-                        <li className="py-2 text-sm">Equipa</li>
-                        <li className="py-2 text-sm">Parceiros</li>
-                    </ul>
-                </div>
-
-                <div>
-                    <h6 className="py-1 font-mono text-xl text-gray-400">Legal</h6>
-                    <ul>
-                        <li className="py-2 text-sm">Contratos e Termos de Serviços</li>
-                        <li className="py-2 text-sm">Politica de Cookies</li>
-                        <li className="py-2 text-sm">Direitos do Consumidor</li>
-                        <li className="py-2 text-sm">Regulamentos e Certificações</li>
-                    </ul>
-                </div>
-
-            </div>
+        <div>
+          <h6 className="py-1 font-mono text-xl text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Contratos e Termos de Serviços</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Politica de Cookies</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Direitos do Consumidor</li>
+            <li className="py-2 text-sm relative  hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:w-full hover:after:h-[4px] hover:after:bg-slate-700">Regulamentos e Certificações</li>
+          </ul>
         </div>
         
-        
-        
-
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- mgsolutions/src/components/Navbar.jsx
+++ mgsolutions/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { TfiClose } from "react-icons/tfi";
 import { TfiAlignRight } from "react-icons/tfi";
 
 function Navbar() {
-
   const [toggle, setToggle] = useState(false);
 
   function openMenu() {
@@ -19,30 +18,54 @@ function Navbar() {
     <>
       <div className="flex items-center justify-between p-8 md:flex-row bg-slate-950 w-full h-full">
         <div>
-          <Link to="/" className="text-white font-mono text-3xl ml-2 flex items-center">
+          <Link
+            to="/"
+            className="text-white font-mono text-3xl ml-2 flex items-center"
+          >
             MGsolutions
           </Link>
         </div>
         <div className="space-x-10">
           <div className="ssm:hidden md:block space-x-1">
-            <Link to="/telecomunicacoes" className="text-white hover:bg-slate-500 rounded-full px-3 py-2 text-xl">
+            <Link
+              to="/telecomunicacoes"
+              className="text-white hover:bg-slate-500 rounded-full px-3 py-2 text-xl"
+            >
               Telecomunicações
             </Link>
-            <Link to="/eletricidade" className="text-white hover:bg-slate-500 rounded-full px-3 py-2 text-xl">
+            <Link
+              to="/eletricidade"
+              className="text-white hover:bg-slate-500 rounded-full px-3 py-2 text-xl"
+            >
               Eletricidade
             </Link>
-            <Link to="/inteligentes" className="text-white hover:bg-sky-700 rounded-full px-3 py-2 text-xl">
+            <Link
+              to="/inteligentes"
+              className="text-white hover:bg-sky-700 rounded-full px-3 py-2 text-xl"
+            >
               Inteligentes
             </Link>
-            <Link to="/contact" className="text-white hover:bg-sky-700 rounded-full px-3 py-2 text-xl">
+            <Link
+              to="/contact"
+              className="text-white hover:bg-sky-700 rounded-full px-3 py-2 text-xl"
+            >
               Contact
             </Link>
           </div>
           <div className="ssm:block md:hidden">
             {toggle ? (
-              <TfiClose onClick={closeMenu} size={30} className="text-white cursor-pointer" />
-            ) : (<TfiAlignRight onClick={openMenu} size={30} className="text-white cursor-pointer" />)}
-
+              <TfiClose
+                onClick={closeMenu}
+                size={30}
+                className="text-white cursor-pointer"
+              />
+            ) : (
+              <TfiAlignRight
+                onClick={openMenu}
+                size={30}
+                className="text-white cursor-pointer"
+              />
+            )}
           </div>
         </div>
       </div>
@@ -51,20 +74,26 @@ function Navbar() {
         {toggle ? (
           <div className="flex justify-between ml-10 pb-10  ">
             <ul className="border-r-2 border-gray-700 pr-10  ">
-              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700  ">Telecomunicações</li>
-              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700 ">Eletricidade</li>
-              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700 ">Inteligentes</li>
-              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700 ">Contacto</li>
+              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700  ">
+                Telecomunicações
+              </li>
+              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700 ">
+                Eletricidade
+              </li>
+              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700 ">
+                Inteligentes
+              </li>
+              <li className="text-white text-xl mb-5 border-b border-grat-700 pb-1 hover:bg-sky-700 ">
+                Contacto
+              </li>
             </ul>
           </div>
         ) : (
           <div></div>
-
         )}
-
       </div>
     </>
-  )
+  );
 }
 
 export default Navbar;
